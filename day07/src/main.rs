287828d608b46b8aacff872efc455e653c99871a
Dan Skorupski <boardwalk@ersatsz.com>
2024-12-07 13:48:34 -0600

    7b?

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,5 +1,7 @@
 #![feature(iterator_try_collect)]
 use anyhow::{bail, Error};
+use indicatif::ProgressIterator as _;
+use rayon::prelude::*;
 
 #[derive(Debug)]
 struct Equation {
@@ -7,48 +9,74 @@ struct Equation {
     args: Vec<usize>,
 }
 
-enum Op {
-    Add,
-    Mul,
-}
+fn do_append_fast(mut res: usize, mut arg_val: usize) -> usize {
+    let mut digits = Vec::new();
 
-fn get_op(arg_i: usize, permute_num: usize) -> Op {
-    if (permute_num >> arg_i) % 2 == 0 {
-        Op::Add
-    } else {
-        Op::Mul
+    while arg_val != 0 {
+        let digit = arg_val % 10;
+        arg_val /= 10;
+
+        digits.push(digit);
     }
-}
 
-fn eval_equation(eqn: &Equation, permute_num: usize) -> usize {
-    let mut res = 0;
-
-    for (arg_i, op_val) in eqn.args.iter().enumerate() {
-        match get_op(arg_i, permute_num) {
-            Op::Add => {
-                res += *op_val;
-            }
-            Op::Mul => {
-                res *= *op_val;
-            }
-        }
+    for digit in digits.iter().rev() {
+        res = res * 10 + *digit;
     }
 
     res
 }
 
-fn equation_satisfiable(eqn: &Equation) -> bool {
-    let num_operators = eqn.args.len();
-    for permute_num in 0..(1 << num_operators) {
-        if eval_equation(eqn, permute_num) == eqn.test_value {
-            return true;
+fn do_append_slow(res: usize, arg_val: usize) -> usize {
+    let mut res_tmp = res.to_string();
+    let arg_tmp = arg_val.to_string();
+    res_tmp.push_str(&arg_tmp);
+    usize::from_str_radix(&res_tmp, 10).unwrap()
+}
+
+fn eval_equation(eqn: &Equation, mut permute_num: usize) -> Option<usize> {
+    let mut res = eqn.args[0];
+
+    for arg_val in eqn.args.iter().skip(1) {
+        let op_num = permute_num & 3;
+        permute_num >>= 2;
+
+        match op_num {
+            0 => {
+                res = res + *arg_val;
+            }
+            1 => {
+                res = res * *arg_val;
+            }
+            2 => {
+                // res = do_append_fast(res, *arg_val);
+            }
+            _ => {
+                return None;
+            }
         }
     }
 
-    false
+    Some(res)
+}
+
+fn equation_satisfiable(eqn: &Equation) -> bool {
+    let num_permutes = 1 << (2 * (eqn.args.len() - 1));
+
+    (0..num_permutes)
+        .into_par_iter()
+        .find_any(|permute_num| {
+            let Some(val) = eval_equation(eqn, *permute_num) else {
+                return false;
+            };
+
+            val == eqn.test_value
+        })
+        .is_some()
 }
 
 fn main() -> Result<(), Error> {
+    assert_eq!(do_append_slow(12, 345), 12345);
+    assert_eq!(do_append_fast(12, 345), 12345);
     let mut eqns = Vec::new();
 
     for ln in std::io::stdin().lines() {
@@ -73,7 +101,8 @@ fn main() -> Result<(), Error> {
 
     let mut sum = 0;
 
-    for eqn in &eqns {
+    for eqn in eqns.iter().progress() {
+        // println!("testing eqn {eqn:?}");
         if equation_satisfiable(eqn) {
             sum += eqn.test_value;
         }
