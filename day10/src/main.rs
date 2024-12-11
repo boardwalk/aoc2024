c6a91ebc598c012ea32c84b784ea17c5b207a04a
Dan Skorupski <boardwalk@ersatsz.com>
2024-12-10 20:11:01 -0600

    10b

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::{anyhow, Error};
 use ndarray::Array2;
+use std::collections::HashSet;
+use std::fmt::Write as _;
 
 fn get_height(heights: &Array2<char>, pos: (usize, usize)) -> Result<usize, Error> {
     let height = heights[pos];
@@ -25,37 +27,55 @@ fn shift(heights: &Array2<char>, pos: (usize, usize), dr: i64, dc: i64) -> Optio
     }
 }
 
-const PART_TWO: bool = false;
+const PART_TWO: bool = true;
 
 const DELTAS: &[(i64, i64)] = &[(0, 1), (0, -1), (-1, 0), (1, 0)];
 
 struct Work {
     pos: (usize, usize),
+    path: String,
 }
 
-fn eval_trailhead(heights: &Array2<char>, start: (usize, usize)) -> Result<usize, Error> {
+fn amend_path(path: &str, dir_idx: usize) -> String {
+    let mut path = path.to_owned();
+    write!(&mut path, "{dir_idx}").unwrap();
+    path
+}
+
+fn eval_trailhead(heights: &Array2<char>, pos: (usize, usize)) -> Result<usize, Error> {
     let mut seen: Array2<bool> = Array2::default(heights.raw_dim());
     let mut work_queue = Vec::new();
-    work_queue.push(Work { pos: start });
+    let mut paths: HashSet<String> = HashSet::new();
+    work_queue.push(Work {
+        pos,
+        path: String::new(),
+    });
 
     while let Some(work) = work_queue.pop() {
         seen[work.pos] = true;
         let height = get_height(heights, work.pos)?;
-        for (dr, dc) in DELTAS {
-            let Some(new_pos) = shift(heights, work.pos, *dr, *dc) else {
+        if height == 9 {
+            paths.insert(work.path.clone());
+        }
+
+        for (dir_idx, (dr, dc)) in DELTAS.iter().enumerate() {
+            let Some(pos) = shift(heights, work.pos, *dr, *dc) else {
                 continue;
             };
 
-            let new_height = get_height(heights, new_pos)?;
+            let new_height = get_height(heights, pos)?;
 
-            if !seen[new_pos] && new_height == height + 1 {
-                work_queue.push(Work { pos: new_pos });
+            if new_height == height + 1 {
+                work_queue.push(Work {
+                    pos,
+                    path: amend_path(&work.path, dir_idx),
+                });
             }
         }
     }
 
     if PART_TWO {
-        Ok(0)
+        Ok(paths.len())
     } else {
         let mut score = 0;
         for (pos, val) in heights.indexed_iter() {
