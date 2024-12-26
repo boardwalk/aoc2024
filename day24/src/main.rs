f5a62debf43b87e733495dd39cf4dada9dc0d05f
Dan Skorupski <boardwalk@ersatsz.com>
2024-12-26 10:52:11 -0600

    renaming

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -11,34 +11,34 @@ pub enum Operator {
 
 #[derive(Debug)]
 struct Gate {
-    left_input: usize,
+    left_wire_id: usize,
     operator: Operator,
-    right_input: usize,
-    output: usize,
+    right_wire_id: usize,
+    output_wire_id: usize,
 }
 
 #[derive(Debug)]
 struct Circuit {
     initial_state: Vec<Option<bool>>,
     gates: Vec<Gate>,
-    x_wires: Vec<usize>,
-    y_wires: Vec<usize>,
-    z_wires: Vec<usize>,
+    x_wire_ids: Vec<usize>,
+    y_wire_ids: Vec<usize>,
+    z_wire_ids: Vec<usize>,
 }
 
-fn get_wire_id(wires: &mut HashMap<String, usize>, name: &str) -> usize {
-    match wires.get(name) {
+fn get_wire_id(wire_ids: &mut HashMap<String, usize>, name: &str) -> usize {
+    match wire_ids.get(name) {
         Some(id) => *id,
         None => {
-            let id = wires.len();
-            wires.insert(name.to_owned(), id);
+            let id = wire_ids.len();
+            wire_ids.insert(name.to_owned(), id);
             id
         }
     }
 }
 
-fn get_io_wires(wires: &HashMap<String, usize>, prefix: &str) -> Vec<usize> {
-    let mut z_names: Vec<_> = wires
+fn get_wire_ids_by_prefix(wire_ids: &HashMap<String, usize>, prefix: &str) -> Vec<usize> {
+    let mut z_names: Vec<_> = wire_ids
         .iter()
         .filter(|(name, _id)| name.starts_with(prefix))
         .collect();
@@ -49,14 +49,14 @@ fn get_io_wires(wires: &HashMap<String, usize>, prefix: &str) -> Vec<usize> {
     z_names.iter().map(|(_name, id)| **id).collect()
 }
 
-fn parse_gate(s: &str, wires: &mut HashMap<String, usize>) -> Result<Gate, Error> {
+fn parse_gate(s: &str, wire_ids: &mut HashMap<String, usize>) -> Result<Gate, Error> {
     let tokens: Vec<_> = s.split_ascii_whitespace().collect();
 
     if tokens.len() != 5 {
         bail!("gate not enough tokens")
     }
 
-    let left_input = get_wire_id(wires, tokens[0]);
+    let left_input = get_wire_id(wire_ids, tokens[0]);
     let operator = match tokens[1] {
         "AND" => Operator::And,
         "OR" => Operator::Or,
@@ -64,29 +64,29 @@ fn parse_gate(s: &str, wires: &mut HashMap<String, usize>) -> Result<Gate, Error
         _ => bail!("gate invalid operator"),
     };
 
-    let right_input = get_wire_id(wires, tokens[2]);
+    let right_input = get_wire_id(wire_ids, tokens[2]);
     if tokens[3] != "->" {
         bail!("gate missing arrow");
     }
 
-    let output = get_wire_id(wires, tokens[4]);
+    let output = get_wire_id(wire_ids, tokens[4]);
     Ok(Gate {
-        left_input,
+        left_wire_id: left_input,
         operator,
-        right_input,
-        output,
+        right_wire_id: right_input,
+        output_wire_id: output,
     })
 }
 
 fn parse_initial_value(
     s: &str,
-    wires: &mut HashMap<String, usize>,
+    wire_ids: &mut HashMap<String, usize>,
 ) -> Result<(usize, bool), Error> {
     let (k, v) = s
         .split_once(": ")
         .ok_or_else(|| anyhow!("bad initial value"))?;
 
-    let k = get_wire_id(wires, k);
+    let k = get_wire_id(wire_ids, k);
 
     let v = match v {
         "0" => false,
@@ -98,10 +98,10 @@ fn parse_initial_value(
 }
 
 fn read_circuit(rd: impl BufRead) -> Result<Circuit, Error> {
+    let mut wire_ids = HashMap::new();
     let mut initial_values = HashMap::new();
     let mut gates = Vec::new();
     let mut in_gates = false;
-    let mut wires = HashMap::new();
 
     for ln in rd.lines() {
         let ln = ln?;
@@ -112,34 +112,34 @@ fn read_circuit(rd: impl BufRead) -> Result<Circuit, Error> {
         }
 
         if in_gates {
-            gates.push(parse_gate(&ln, &mut wires)?);
+            gates.push(parse_gate(&ln, &mut wire_ids)?);
         } else {
-            let (k, v) = parse_initial_value(&ln, &mut wires)?;
+            let (k, v) = parse_initial_value(&ln, &mut wire_ids)?;
             initial_values.insert(k, v);
         }
     }
 
-    let x_wires = get_io_wires(&wires, "x");
-    let y_wires = get_io_wires(&wires, "y");
-    let z_wires = get_io_wires(&wires, "z");
+    let x_wire_ids = get_wire_ids_by_prefix(&wire_ids, "x");
+    let y_wire_ids = get_wire_ids_by_prefix(&wire_ids, "y");
+    let z_wire_ids = get_wire_ids_by_prefix(&wire_ids, "z");
 
-    let mut initial_state = vec![None; wires.len()];
+    let mut initial_state = vec![None; wire_ids.len()];
     for (id, val) in &initial_values {
         initial_state[*id] = Some(*val);
     }
 
     Ok(Circuit {
         gates,
-        x_wires,
-        y_wires,
-        z_wires,
+        x_wire_ids,
+        y_wire_ids,
+        z_wire_ids,
         initial_state,
     })
 }
 
-fn propagate(wires: &[Option<bool>], gate: &Gate) -> Option<bool> {
-    let left = wires[gate.left_input]?;
-    let right = wires[gate.right_input]?;
+fn propagate(wire_values: &[Option<bool>], gate: &Gate) -> Option<bool> {
+    let left = wire_values[gate.left_wire_id]?;
+    let right = wire_values[gate.right_wire_id]?;
 
     let val = match gate.operator {
         Operator::And => left && right,
@@ -151,14 +151,14 @@ fn propagate(wires: &[Option<bool>], gate: &Gate) -> Option<bool> {
 }
 
 fn solve_circuit(circuit: &Circuit) -> Result<Vec<bool>, Error> {
-    let mut wires = circuit.initial_state.clone();
-    let mut remaining = wires.iter().filter(|v| v.is_none()).count();
+    let mut wire_values = circuit.initial_state.clone();
+    let mut remaining = wire_values.iter().filter(|v| v.is_none()).count();
     while remaining > 0 {
         let mut new_remaining = remaining;
         for gate in &circuit.gates {
-            if wires[gate.output].is_none() {
-                if let Some(val) = propagate(&wires, gate) {
-                    wires[gate.output] = Some(val);
+            if wire_values[gate.output_wire_id].is_none() {
+                if let Some(val) = propagate(&wire_values, gate) {
+                    wire_values[gate.output_wire_id] = Some(val);
                     new_remaining -= 1;
                 }
             }
@@ -170,16 +170,16 @@ fn solve_circuit(circuit: &Circuit) -> Result<Vec<bool>, Error> {
         remaining = new_remaining;
     }
 
-    let wires = wires.into_iter().map(Option::unwrap).collect();
+    let wires = wire_values.into_iter().map(Option::unwrap).collect();
 
     Ok(wires)
 }
 
-fn calculate(wires: &[bool], circuit: &Circuit) -> usize {
+fn get_value(wire_values: &[bool], wire_ids: &[usize]) -> usize {
     let mut output = 0;
 
-    for io_wire in &circuit.z_wires {
-        let val = wires[*io_wire];
+    for src_wire in wire_ids {
+        let val = wire_values[*src_wire];
         let val = usize::from(val);
         output = output * 2 + val;
     }
@@ -189,8 +189,8 @@ fn calculate(wires: &[bool], circuit: &Circuit) -> usize {
 
 fn main() -> Result<(), Error> {
     let circuit = read_circuit(std::io::stdin().lock())?;
-    let wires = solve_circuit(&circuit)?;
-    let output = calculate(&wires, &circuit);
+    let wire_values = solve_circuit(&circuit)?;
+    let output = get_value(&wire_values, &circuit.z_wire_ids);
     println!("{output}");
     Ok(())
 }
